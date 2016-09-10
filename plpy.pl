e1e53e377bb8dd825883df5b498024484dadbc01
Marlene Baquiran <marlene.baquiran@gmail.com>
2016-09-10 21:27:45 +1000

    SET02: implemented handlers for answer5-6, iota, but not all set2 features

--- plpy.pl
+++ plpy.pl
@@ -15,27 +15,44 @@ while ($line = <>) {
         # Blank & comment lines can be passed unchanged
 
         print $line;
-    } elsif ($line =~ /^\s*print\s*"([^\$]*)\\n"[\s;]*$/) {
-        $print_text = $1;
 
+    # print "hello world\n";
+    } elsif ($line =~ /^\s*print\s*"([^\$]*)\\n"[\s;]*$/) {
         # \s means whitespace:
         #       print     "blah blah blah\n"     ;
         # Python's print adds a new-line character by default
         # so we need to delete it from the Perl print statement
 
-        print "print(\"$print_text\")\n";
+        print "print(\"$1\")\n";
+
+    # print "$var\n";
     } elsif ($line =~ /^\s*print\s*"\$([A-Za-z0-9]+)\\n"[\s;]*$/) { # shitty
        # hack
-        print "print($1)\n";
+        $replace = "print($1)";
+        $line =~ s/print.*/$replace/;
+        print $line;
+
+    # print $var, "\n";
     } elsif ($line =~ /^\s*print\s*[^"]([^,]*)[^"],\s"\\n"[\s;]*$/) { # shitty
        # hack
         print "print($1)\n";
+        ## do the same thing here later..
+
+    # a = num;
     } elsif ($line =~ /^\s*\$([A-Za-z0-9]+)\s*=\s(.*);$/) {
         $line =~ s/\$//g;
+        $line =~ s/;//g;
         print $line;
-        # $answer = 42;
-        # print "Translating var assignment:\n";
+    # if (a > 3) {
+    } elsif ($line =~ /^\s*if\s\((.*)\)\s{/ || $line =~ /^\s*while\s\((.*)\)\s{/) {
+        $line =~ s/\(//g;
+        $line =~ s/\)//g;
+        $line =~ s/\s{/:/g;
+        $line =~ s/\$//g;
+        print $line;
 
+    # }
+    } elsif ($line =~ /}/) {
     } else {
         # Lines we can't translate are turned into comments
         # print "No match for:\n";
