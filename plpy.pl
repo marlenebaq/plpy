5eebd8a4d2fcb3d9f4c0ab8f8408306194df302f
Marlene Baquiran <marlene.baquiran@gmail.com>
2016-09-26 00:58:25 +1000

    SET04: all tests passed but size.pl

--- plpy.pl
+++ plpy.pl
@@ -6,11 +6,15 @@
 
 # WHAT IS PYTHON EQUIVALENT OF PERL exit AND WHY ARE THERE SO MANY?
 
-# use constant {
-#     INT => 'int',
-#     FLOAT => 'float',
-#     STRING => 
-# };
+use constant {
+    _INT => "int",
+    _FLOAT => "float",
+    _STRING => "string",
+};
+
+my @py_code;
+my @py_header;
+my @py_imports;
 
 my %imports = (
     "sys" => "0",
@@ -20,67 +24,75 @@ my %imports = (
 my %lookup = (
     "<STDIN>" => "sys.stdin",
     "<>" => "fileinput.input()",
+    "ARGV" => "sys.argv",
 
+    "elsif" => "elif",
+
+    "eq" => "==",
     "&&" => "and",
     "||" => "or",
     "!" => "not ",
 );
 
-my %all_vars = ();
-# "var_name" => "type",
-
+my %typeof = ();
+my %var_assignment = ();
+my $line_num = 0;
 # MAIN
 while (my $pl_line = <>) {
-    our @py_code; # TODO: stop using our wtf
-    our @py_header;
-    translate_pl_line($pl_line);
+    translate_pl_line($pl_line, $#py_code+1);
+}
+
+# TODO: change
+if (scalar @py_imports > 0) {
+    push (@py_header, (join('', "import ", join(', ', @py_imports)) . "\n"));
 }
 my @py_lines = join('', @py_header,@py_code);
 print @py_lines;
 
 sub translate_pl_line {
-    my ($line) = @_;
+    my ($line, $line_num) = @_;
     my $whitespace = ($line =~ /^(\s*)/g)[0];
     my $contents = ($line =~ /([^\s].*)/g)[0];
     my $no_line = 0;
     my $array_ref = \@py_code;
 
-    if ($contents) {
+    if ($contents && $contents =~ m{\w+}) {
         $contents = prelim_syntax_cleanup($contents);
         if ($contents =~ /^#!/ && $. == 1) {
             # translate #! line
             $array_ref = \@py_header;
             $contents = "#!/usr/local/bin/python3.5 -u\n";
-        } elsif ($contents =~ /#/) {
+        } elsif ($contents =~ /^#/) {
             # comments pass through unchanged
             $contents .= "\n";
         } elsif ($contents =~ /print\s*(.*)/) {
             # handle print statements
             # $1 - all content
             $contents = handle_print($1);
-        } elsif ($contents =~ /(if|elif|while)\s*\((.*<?>?.*)\)\s*{/) {
-            # NOTE: does not take while (< ... >)
-            # NEED TO FIX.
+        } elsif ($contents =~ /(if|elsif|while)\s*\((.*)\)/) {
             # handle if and whiles
             # $1 - condition type
             # $2 - condition
-            # TODO: handle else {
-            # print "CONDITIONAL---$contents\n";
             $contents = handle_conditional($1, $2);
             #foreach $i (0..4) {
-        } elsif ($contents =~ /else/) {
-            $contents = "else:\n"
-        } elsif ($contents =~ /for(each)?\s+\$(\w+)\s+(.+)\s*{/) {
+        } elsif ($contents =~ m{for(each)?\s+(.+)\s+(.+)}) {
             # handle foreach
             # $1 - iterator
             # $2 - iterable
             $contents = handle_foreach($2, $3);
-        } elsif ($contents =~ /\$(\w+)((\+|-)){2}$/i) {
-            # handle (in|de)crements
-            # $1 - var to (in|de)crement
-            # $2 - operator (+|-)
-            $contents = handle_inde_crement($1, $2);
-        } elsif ($contents =~ /(last|next)$/) {
+        } elsif ($contents =~ m{(.*) =~ s/(.*)/(.*)/(.*)}) {
+            # this precedes = var assignment
+            # $1 - expression to sub
+            # $2 - find
+            # $3 - replace
+            # $4 - flags
+            $contents = handle_re_sub($1, $2, $3, $4)
+        } elsif ($contents =~ m{(([^\s]+)\s*=\s*.+)}) {
+            # handle var
+            # $2 - var being assigned
+            # $3 - mutation
+            $contents = handle_variable($1, $2, $line_num)
+        } elsif ($contents =~ /^(last|next)$/) {
             # handle last/next
             # $1 - all content
             $contents = handle_last_next($1);
@@ -88,23 +100,6 @@ sub translate_pl_line {
             # handle chomp
             # $1 - var to chomp
             $contents = handle_chomp($1);
-        } elsif ($contents =~ /while \(\$(\w+) = <(.*)>\)/i) {
-            # TODO: convert while (<>) to for line in fileinput.input()
-            # handle read in
-            # $1 - var to assign
-            # $2 - 'filehandle' (change wording...)
-            $contents = handle_while_read_in($1, $2);
-        } elsif ($contents =~ /\$(.*) =~ s\/(.*)\/(.*)\/(.*)/) {
-            # keep in mind input will be s///, probably not s{}{}
-            # TODO: should all whitespace be \s, or just use ' ' for easier
-            # reading?
-            # $1 - expression to sub
-            # $2 - find
-            # $3 - replace
-            # $4 - flags
-            $contents = handle_re_sub($1, $2, $3, $4)
-        } elsif ($contents =~ /}/) {
-            $no_line = 1;
         }
 
         $contents =~ /^\s*(.*)/;
@@ -112,6 +107,7 @@ sub translate_pl_line {
         $contents = cleanup_remaining_syntax($1);
     } else {
         $contents = "";
+        $no_line = 1;
     }
     unless ($no_line) {
         $line = $whitespace . $contents;
@@ -125,47 +121,28 @@ sub translate_pl_line {
 sub prelim_syntax_cleanup {
     # cleanup ; (trailing), ..
     my ($contents) = @_;
-    $contents =~ s{;$}{}g; # remove only trailing semicolons
-    $contents =~ s{elsif}{elif}g;
-    # TODO: optimise/shorten/safen (replaces any ..)
-    if ($contents =~ /\[\w*..\w*\]/) {
-        $contents =~ s{\.\.}{:};
-    }
-    # TODO: refactor!!! spaghetti!!!
-    if ($contents =~ /\((.*)\.\.(.*)\)/) {
-        my $start = "";
-        my $end = "";
-        unless ($2 eq "\$#ARGV") {
-            $start = $1;
-            $end = $2 + 1;
-        } elsif ($1 == 0) {
-            $end = "len(sys.argv) - 1";
-        }
-        my $replace;
-        if ($start ne "") {
-            $replace = "range($start, $end)";
-        } else {
-            # implied range(0, end)
-            $replace = "range($end)";
-        }
-        $contents =~ s{\(.*\.\..*\)}{$replace};
-    }
-    if ($contents =~ /\@ARGV/) {
-        $contents =~ s{\@ARGV}{sys.argv[1:]}g;
-        try_import("sys");
-    }
-    if ($contents =~ /join\((.*)\)/) {
-        my @join_args = split(', ', $1);
-        my $py_join = "$join_args[0].join($join_args[1])";
-        $contents =~ s{join.*\)}{$py_join};
-    }
-    if ($contents =~ /split\((.*)\)/) {
-        my @split_args = split(', ', $1);
-        my $py_split = "$split_args[1].split($split_args[0])";
-        $contents =~ s{split.*\)}{$py_split};
-    }
-
+    $contents =~ s{;$}{}; # remove only trailing semicolons
+    $contents =~ s{\s*\{$}{:}; # substitute only trailing {
+    $contents =~ s{\}\s*}{}; # substitute only leading }
     return $contents;
+}
+
+sub handle_variable {
+    my ($contents, $var, $line_num) = @_;
+    $var_assignment{$var} = $line_num;
+    $typeof{$var} = _FLOAT if ($var =~ m{\d});
+    return $contents;
+}
+
+# TODO: only modify part so split AND join work
+sub handle_join_split {
+    my ($func, $args_str) = @_;
+    my @args = split(', ', $args_str);
+    my $replace;
+    if ($func eq "join") {
+        return "$args[0].join($args[1])";
+    }
+    return "$args[1].split($args[0])";
 }
 
 sub handle_print {
@@ -184,7 +161,6 @@ sub handle_print {
         $contents = $1;
         $contents =~ s{\$}{}g;
     } else {
-        # TODO: allow "--$var--"
         my @components = split(/\s+/, $contents);
         my @vars;
         foreach $component (@components) {
@@ -212,15 +188,30 @@ sub handle_print {
 
 sub handle_conditional {## $line =~ s{(.*)\)}{$1}xms; meaning?
     my ($statement_type, $condition) = @_;
+    if ($condition =~ m{\$(\w+) = (<.*>)}) {
+        $contents = handle_while_read_in($1, $2);
+        return $contents;
+    }
 
     # remove $ from vars
-    $condition =~ s{\$}{}g;
-    # change eq to == (value test, not identity)
-    $condition =~ s{eq}{==}g;
-    # logical operators
-    $condition =~ s{&&}{and}g;
-    $condition =~ s{\|\|}{or}g;
-    $condition =~ s{!}{not }g;
+    $condition =~ s{(eq|&&|\|\|!)}{lookup_syntax($1)}ge;
+    $statement_type =~ s{elsif}{elif}g;
+    if ($condition =~ m{(\$\w+) ([\+\-\/><]+|<=|>=)} || $condition =~ m{==
+       \d+}) {
+        my $var = 1;
+        my $index = $var_assignment{$1};
+        my $last_assignment = $py_code[$index];
+        # this looks back to cast a previous line as a float
+        # if we see that usage is appropriate for a float
+        # but it was not originally explicit
+        if ((!exists $typeof{$var} || $typeof{$var} ne _FLOAT) &&
+           $last_assignment
+           !~ m{\d} &&
+           $last_assignment =~ m{(.*) = (.*)}) {
+            $py_code[$index] = "$1 = float($2)\n";
+            $typeof{$var} = _FLOAT;
+        }
+    }
 
     return "$statement_type " . $condition . ":\n";
 }
@@ -232,11 +223,6 @@ sub handle_foreach {
     }
     $iterable =~ s{\s+$}{};
     return "for $iterator in $iterable:\n";
-}
-
-sub handle_inde_crement {
-    my ($var, $operator) = @_;
-    return "$var $operator= 1\n";
 }
 
 sub handle_last_next {
@@ -254,15 +240,8 @@ sub handle_chomp {
 }
 
 sub handle_while_read_in {
-    # TODO: change varname '$stream' (is that really what it refers to?)
     my ($var, $stream) = @_;
-    if ($stream eq "STDIN") {
-        try_import("sys");
-    } elsif ($stream eq "") {
-        try_import("fileinput");
-    }
-    # it is not necessarily clear that this uses sys...
-    return "for $var in $lookup{\"<$stream>\"}:\n";
+    return "for $var in $stream:\n";
 }
 
 sub handle_re_sub {
@@ -271,41 +250,94 @@ sub handle_re_sub {
     return "$expr = re.sub(r'$find', '$replace', $expr)";
 }
 
+# handles syntax that is not exclusive to line format
 sub cleanup_remaining_syntax {
     my ($contents) = @_;
 
-    if ($contents !~ /^\s*#/) {
+    # remove $, @ and % variable markers
+    # if not a comment (or formatted string?)
+    #  && $contents !~ m{print}
+    if ($contents !~ m{^\s*#}) {
         $contents =~ s{[\$\@;]}{}g;
-        # do not replace %s, %d, %f
-        # TODO: fix this is retarded
-        $contents =~ s{%([a-ceg-rt-z]+)}{$1}gi;
-    }
-    # note -> takes ANYTHING in <>, even in print etc.
-    if ($contents =~ /(<.*>)/) {
-        # TODO: change name
-        # TODO: check if this is a good assumption
-        # replaces stuff like STDIN => sys.stdin
-        my $replace = $lookup{"$1"};
-        if ($replace =~ /sys/) {
-            $replace .= ".readline()";
-            try_import("sys");
+        if ($contents !~ m{print}) {
+            $contents =~ s{%(\w+)}{$1}gi;
         }
-        $contents =~ s{<.*>}{$replace};
-    }
-    $contents =~ s{<(.*)>}{}g;
 
+        if ($contents =~ m{[^#](ARGV)}) {
+            my $pl_syntax = $1;
+            my $py_syntax = lookup_syntax($pl_syntax);
+            if ($contents =~ m{ARGV\[(.+)\]}) {
+                $contents =~ s{ARGV\[(.+)\]}{$py_syntax\[$1 + 1\]};
+            } else {
+                $contents =~ s{ARGV}{$py_syntax\[1:\]};
+            }
+        }
+        # note -> takes ANYTHING in <>, even in print etc.
+        if ($contents =~ m{(<.*>)}) {
+            my $pl_syntax = $1;
+            my $py_syntax;
+            # need sys.stdin.readline() if not a while loop
+            if ($pl_syntax =~ m{STDIN}) {
+                $py_syntax = lookup_syntax($pl_syntax);
+            } else {
+                $py_syntax = lookup_syntax($pl_syntax);
+            }
+            $py_syntax .= ".readline()" if $contents !~ m{for.*};
+            $contents =~ s{<.*>}{$py_syntax};
+        }
+        # TODO: refactor!!! spaghetti!!!
+        # (this..that) => range(this, that)
+        if ($contents =~ m{(\w*)\.\.([^:]*)}g) {
+            my $start = $1;
+            my $end = $2;
+            if ($end !~ m{#ARGV}) {
+                $end = $2 + 1;
+            } elsif ($start == 0) {
+                $start = "";
+                $end = "len(sys.argv) - 1";
+            }
+            my $replace;
+            if ($start eq "") {
+                # implied range(0, end)
+                $replace = "range($end)";
+            } else {
+                $replace = "range($start, $end)";
+            }
+            $contents =~ s{\w*\.\.[^:]*}{$replace};
+        }
+        # is this necessary?
+        if ($contents =~ /\[\w*..\w*\]/) {
+            $contents =~ s{\.\.}{:};
+        }
+        if ($contents =~ m{(\w+)(\+|-){2}$}i) {
+            # $1 - variable
+            # $2 - +- operator
+            $contents = "$1 $2= 1\n";
+        }
+        if ($contents =~ /(split|join)\((.*)\)/) {
+            $replace = handle_join_split($1, $2);
+            $contents =~ s{(split|join)\((.*)\)}{$replace}e;
+        }
+    }
+
+    # why new line?
     return "$contents\n";
+}
+
+sub lookup_syntax {
+    my ($pl_syntax) = @_;
+    my $py_syntax = $lookup{$pl_syntax};
+    my $module;
+    if ($py_syntax =~ m{(sys|fileinput)}) {
+        try_import($1);
+    }
+    return $py_syntax;
 }
 
 sub try_import {
     my ($module) = @_;
     if ($imports{$module} == 0) {
-        # TODO: import a, b, c -> not import a, import b, import c
-        # simply create @modules array sorted alphabetically,
-        # which is pushed onto @py_header
-        # push @modules, $module
-        # sort @modules
-        push @py_header, "import $module\n";
+        push @py_imports, "$module";
         $imports{$module} = 1;
     }
 }
