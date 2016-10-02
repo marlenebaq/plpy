a68f75937dfe88ac1f5a5cb0faaaa41f01654bfe
Marlene Baquiran <marlene.baquiran@gmail.com>
2016-10-03 00:16:53 +1100

    SET02: Add handling for bitwise operators

--- plpy.pl
+++ plpy.pl
@@ -36,48 +36,54 @@ sub lex {
         $$lineData =~ s{^(#!).*}{} and return ("SHEBANG", $1);
     }
 
-    $$lineData =~ s{;$}{}; # remove trailing ;
+    # remove any trailing SEMICOLONS
+    $$lineData =~ s{;$}{};
+
+    # preserve any meaningful WHITESPACE
     $$lineData =~ s{^(\s{4})}{} and return ("WHITESPACE", $1);
     $$lineData =~ s{^(#.*)}{} and return ("COMMENT", $1);
 
+    # 'fundamental' STATEMENTS
     $$lineData =~ s{^\s*print\s*}{} and return ("PRINT", undef);
     $$lineData =~ s{^\s*(if)\s*}{} and return ("IF", $1);
     $$lineData =~ s{^\s*(elsif)\s*}{} and return ("ELSIF", $1);
     $$lineData =~ s{^\s*(else)\s*\{}}{} and return ("ELSE", $1);
     $$lineData =~ s{^\s*(while|foreach|for)\s*}{} and return ("LOOP_TYPE", $1);
 
-    $$lineData =~ s{^\s*\.\.}{} and return ("RANGE", undef);
-
+    # quote-surrounded STRINGS
     $$lineData =~ s{^\s*(["']{2})}{} and return ("EMPTY_STRING", $1);
     $$lineData =~ s{^\s*(["'].+?["'])}{} and return ("SENTENCE", $1);
 
     # OPERATORS & VARIABLES/NUMBERS
-    $$lineData =~ s{^\s*(=|!)~}{} and return ("MATCH_OPERATOR", $1);
+    $$lineData =~ s{^\s*([=!]~)}{} and return ("MATCH_OP", $1);
     $$lineData =~ s{^\s*([\+-]){2}}{} and return ("CREMENT", $1);
-    $$lineData =~ s{^\s*(==|eq)}{} and return ("EQ", $1);
-    $$lineData =~ s{^\s*(!=|ne)}{} and return ("NE", $1);
-    $$lineData =~ s{^\s*>=}{} and return ("GTE", undef);
-    $$lineData =~ s{^\s*<=}{} and return ("LTE", undef);
-    $$lineData =~ s{^\s*&&}{} and return ("AND", undef);
-    $$lineData =~ s{^\s*\|\|}{} and return ("OR", undef);
-    if ($$lineData !~ m{^\s*<.*>}) {
-        $$lineData =~ s{^\s*([\n=\*\+\-/\(\)><\{\}%])}{} and return ($1, $1);
+    $$lineData =~ s{^\s*(&|\||\^|<<|>>)}{} and return ("BW_BINARY_OP", $1);
+    $$lineData =~ s{^\s*(\~)}{} and return ("BW_UNARY_OP", $1);
+    $$lineData =~ s{^\s*(==|eq|!=|ne|>=|<=|>|<|<=>)}{} and return ("COMP_OP",
+    $1);
+    $$lineData =~ s{^\s*(&&|\|\|)}{} and return ("LOG_OP", $1);
+    $$lineData =~ s{^\s*(\+|\-|\*|/|%|\*\*)}{} and return ("MATH_OP", $1);
+    $$lineData =~ s{^\s*!}{} and return ("NOT", undef);
+    if ($$lineData !~ m{^\s*<.*>} && $$lineData !~ m{^\s*/.*/}) {
+        $$lineData =~ s{^\s*([\n=\(\)\{\}])}{} and return ($1, $1);
     }
 
     $$lineData =~ s{^\s*(\d+)}{} and return ("NUMBER", $1);
     $$lineData =~ s{^\s*['"]?([\$@%]#?\w+)}{} and return ("VAR", $1);
     $$lineData =~ s{^\s*(\[.*\])}{} and return ("INDEX", $1);
+    $$lineData =~ s{^\s*\.\.}{} and return ("RANGE", undef);
 
     $$lineData =~ s{^\s*chomp}{} and return ("CHOMP", undef);
     $$lineData =~ s{^\s*<(.*)>}{} and return ("INPUT", $1);
     $$lineData =~ s{^\s*(join\s*)}{} and return ("JOIN", $1);
     $$lineData =~ s{^\s*(split\s*)}{} and return ("SPLIT", $1);
 
-    $$lineData =~ s{^\s*(s/.*/.*/[a-z]*)}{}i and return ("SUBS", $1);
+    $$lineData =~ s{^\s*(s/.*/.*/[a-z]*)}{} and return ("SUBS", $1);
     $$lineData =~ s{^\s*(m?/.*/)}{} and return ("MATCH", $1);
     $$lineData =~ s{^\s*last}{} and return ("LAST", undef);
     $$lineData =~ s{^\s*last}{} and return ("NEXT", undef);
     $$lineData =~ s{^\s*exit}{} and return ("EXIT", undef); #
+    $$lineData =~ s{^\s*open\s*(\w+)}{} and return ("OPENF", $1); #
 
     $$lineData =~ s{^(,\s*)}{} and return ("SEPARATOR", $1); # remove
 
